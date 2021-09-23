8e87aa21c763cfd3a2a3aedf6ec09dcd688be4d8
NikoTiN <dmitrii.asenenko@gmail.com>
2021-09-23 17:08:39 +0300

    create errors class

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,13 @@ import jsonwebtoken from 'jsonwebtoken';
 function auth(req, res, next) {
   const { jwt } = req.cookies;
   if (!jwt) {
-    return res.status(403).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
   let payload;
   try {
     payload = jsonwebtoken.verify(jwt, 'some-secret-key');
   } catch (err) {
-    return res.status(403).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
   next();
